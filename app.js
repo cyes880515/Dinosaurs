e82be0c690c4358b974f7e04ed3334408aaa9dba
cyes880515 <48134112+cyes880515@users.noreply.github.com>
2020-10-18 07:04:22 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,92 +1,142 @@
-    $.getJSON("dino.json", function(data) {
-        const Dinosaur = data;        
-        console.log(Dinosaur);
-    })
     // Create Dino Constructor
-    function Dino(name, species,image) {
-        this.name = name;
-        this.species = species;
-        this.image = image;
+    const Dino = function(object, image) {
+        this.object = object;
+        this.image = image;   
     }
     // Create Dino Objects
-    let anklyosaurus = new Dino('anklyosaurus', 'herbavor','url("https://upload.cc/i1/2020/09/12/G3dnvu.png")');
-    let brachiosaurus = new Dino('brachiosaurus', 'herbavor','url("https://upload.cc/i1/2020/09/12/xKHZvE.png")');
-    let elasmosaurus = new Dino('elasmosaurus', 'carnivor','url("https://upload.cc/i1/2020/09/12/xjMRGY.png")');
-    let pteranodon = new Dino('pteranodon', 'carnivor','url("https://upload.cc/i1/2020/09/12/Clm8wg.png")');
-    let stegosaurus = new Dino('stegosaurus', 'herbavor','url("https://upload.cc/i1/2020/09/12/1QmBDN.png")');
-    let triceratops = new Dino('triceratops', 'herbavor','url("https://upload.cc/i1/2020/09/12/9JHyaG.png")');
-    let tyrannosaurusRex = new Dino('tyrannosaurusRex', 'herbavor','url("https://upload.cc/i1/2020/09/12/3ogRLA.png")');
-    let pigeon = new Dino('pigeon', 'omnivor','url("https://upload.cc/i1/2020/09/12/7UCK2k.png")');
+    // get JSON file and build constructor    
+    var tiles = ["initial"];
+    var getFile = function (callback, UserData) {        
+        $.getJSON("dino.json")
+        .done(function(data) {
+            const Dinosaur = data;
+            // Create Dino Objects
+            for(i = 0; i < 8; i++) {
+                let name = Dinosaur["Dinos"][i];
+                tiles[i] = new Dino(name, 'images/'+ name.species + '.png');
+            }
+            
+            tiles.splice(4,0,new Human(UserData))
+            callback();
+        });   
+    }; 
     // Create Human Object
-    let human = new Dino('', '','url("https://upload.cc/i1/2020/09/12/hfKSz0.png")');
-    // Use IIFE to get human data from form
-    
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-    const tiles = [
-        anklyosaurus,
-        brachiosaurus,
-        elasmosaurus,
-        pteranodon,
-        human,
-        stegosaurus,
-        triceratops,
-        tyrannosaurusRex,
-        pigeon
-     ]
-    // Rearrange the Array    
-    function Rearrange(array) {
-        for (let i = array.length -1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            if ((i === 4) || (j === 4)) continue;
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+    const Human = function(object) {
+        this.name = object.name;
+        this.heightFeet = object.heightFeet;
+        this.heightInches = object.heightInches;
+        this.weight = object.weight;
+        this.diet = object.diet;
     }
-    // Add tiles to DOM
+    // Use IIFE to get human data from form
+
+
+    // Create Dino Compare Method 1
+// first fact: comparing weights
+Dino.prototype.weightFact = function() {
+    let weightRatio= Math.round(this.object.weight / tiles[4].weight).toFixed(2);
+    if (weightRatio > 1) {
+        return `${this.object.species} used to weigh around ${weightRatio} times more than you!!`;
+    } else {
+        return `You are ${1 / weightRatio} times heavier than ${this.object.species} was`;
+    }
+};
+// second fact: comparing heights
+Dino.prototype.heightFact = function() {
+    let heightRatio = this.object.height / (tiles[4].heightFeet*12 + tiles[4].heightInches);
+    heightRatio = Math.round(heightRatio).toFixed(2);
+    if (heightRatio > 1) {
+        return `${this.object.species} was around ${heightRatio} times taller than you!!`
+    } else {
+        return `You are ${1 / heightRatio} times larger than ${this.object.species} used to be`;
+    }
+}
+// third fact: comparing diet
+Dino.prototype.dietFact = function() {
+    if (tiles[4].diet === this.object.diet) {
+        return `Just like you, ${this.object.species} was also ${this.object.diet}`;
+    } else {
+        return `Unlike you, ${this.object.species} was ${this.object.diet}`;
+    }
+}
+
+// fourth fact: timeperiod
+Dino.prototype.timeFact = function() {
+    return `${this.object.species} existed in the ${this.object.when} time period`;
+}
+
+// fifth fact: location related
+Dino.prototype.locationFact = function() {
+    return `The prime location where ${this.object.species} was found was ${this.object.location}`;
+}
+
+// sixth fact: given fact
+Dino.prototype.givenFact = function() {
+    return this.object.fact;
+}
+Dino.prototype.getfact = function() {
+    let randomNumber = Math.ceil(Math.random() * 6);
+    switch(randomNumber) {
+        case 1:
+            console.log(this); 
+            return(this.weightFact());
+        case 2:
+            console.log(this);  
+            return(this.heightFact());
+        case 3:
+            console.log(this);
+            return(this.dietFact());
+        case 4:
+            console.log(this);
+            return(this.timeFact());
+        case 5:
+            console.log(this);
+            return(this.locationFact());
+        case 6:
+            console.log(this);
+            return(this.givenFact());
+    }
+}
+    // Generate Tiles for each Dino in Array
+    var innerHTML = function() {
+        let grid = document.getElementById("grid");
+        let hide = document.getElementById("dino-compare");
+        let msg = document.getElementById("grid").textContent;
+        // Remove form
+        hide.style.display = "none";
+        for (let index = 0; index < 9; index++) {
+            if (index === 4) {
+                msg+=`
+                <div class="grid-item">
+                <h4>${tiles[index].name}</h4>
+                    <img src="./images/human.png">
+                </div>`;
+                continue;
+            }
+            msg+=`
+            <div class="grid-item">
+                <h4>${tiles[index].object.species}</h4>
+                <img src="./images/${tiles[index].object.species}.png">
+                <p>${tiles[index].getfact()}
+            </div>`;
+        }
+        grid.innerHTML = msg;
+    }     
 
     // Remove form from screen
 
-
+// Initial web
 // On button click, prepare and display infographic
-    var btn = document.getElementById("btn");
-    btn.onclick = function() {
-        let hide = document.getElementById("dino-compare");
-        let name = document.getElementById("name");
-        let gird = document.getElementById("grid");
-        // Remove form
-        hide.style.display = "none";
-        // Add grid into the web
-        grid.style.margin = "0 auto";
-        grid.style.width = "1000px";
-        grid.style.height = "800px";
-        grid.innerHTML =`
-        <div class="grid-item">></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>
-        <div class="grid-item"></div>`;
-        // Rearrange the array
-        Rearrange(tiles);
-        // Fill in Random dinosaur data and image
-        for(i = 0; i<tiles.length;i++) {
-            let s = document.getElementsByClassName("grid-item");
-            s[i].innerHTML = tiles[i].name + tiles[i].species;
-            s[i].style.backgroundImage = tiles[i].image;
-            s[i].style.fontFamily = "'Oswald', sans-serif";
+document.getElementById("btn").onclick = function() {
+    let human = (function() {
+        return {
+            name: document.querySelector('#name').value,
+            heightFeet: parseInt(document.querySelector('#feet').value),
+            heightInches: parseInt(document.querySelector('#inches').value),
+            weight: parseInt(document.querySelector('#weight').value),
+            diet: document.querySelector('#diet').value
         }
-    }
+    })();
+    getFile(innerHTML, human);
+
+}
